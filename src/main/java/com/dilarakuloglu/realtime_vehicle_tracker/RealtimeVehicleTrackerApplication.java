aee371b6724e587badbb015ad5ccea341fdc4e93
dilara <dilarakuloglu74@gmail.com>
2026-07-09 09:47:25 +0300

    initial commit

---
+++ src/main/java/com/dilarakuloglu/realtime_vehicle_tracker/RealtimeVehicleTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dilarakuloglu.realtime_vehicle_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeVehicleTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeVehicleTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dilarakuloglu/realtime_vehicle_tracker/RealtimeVehicleTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilarakuloglu.realtime_vehicle_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeVehicleTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
